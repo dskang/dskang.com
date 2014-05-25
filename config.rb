5530b4c25afb14e25d724c5fcc27006ef1703c4b
Dan Kang <dan@dskang.com>
2014-05-25 11:53:10 -0700

    activate pretty URLs

--- config.rb
+++ config.rb
@@ -53,9 +53,12 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+# Pretty URLs (Directory Indexes)
+activate :directory_indexes
+
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
+  # Minify CSS on build
   activate :minify_css
 
   # Minify Javascript on build
