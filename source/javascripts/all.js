8a983dde3d3165ce5e89a99461fb865d2df85601
Dan Kang <dan@dskang.com>
2015-03-19 12:53:09 -0400

    Toggle details with button on mobile

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,18 @@
 //= require_tree .
+
+$(document).ready(function() {
+  $(".toggle-details").on("click", function() {
+    var button = $(this);
+    button.blur();
+    button.next(".event-details").toggle();
+    var text = {
+      show: "Show Details",
+      hide: "Hide Details"
+    };
+    if (button.text() === text.show) {
+      button.text(text.hide);
+    } else {
+      button.text(text.show);
+    }
+  });
+});
