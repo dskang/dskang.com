8519ebc2d2938c55b2b4b78ad8550d4f8671415d
Dan Kang <dan@dskang.com>
2015-03-19 18:36:17 -0400

    fade in event details on scroll

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,6 +1,7 @@
 //= require_tree .
 
 $(document).ready(function() {
+  // Toggle event details on click
   $(".toggle-details").on("click", function() {
     var button = $(this);
     button.blur();
@@ -14,5 +15,16 @@ $(document).ready(function() {
     } else {
       button.text(text.show);
     }
+  });
+
+  // Fade in event details
+  $(window).scroll(function() {
+    $(".event-row").each(function() {
+      var bottomOfRow = $(this).offset().top + $(this).outerHeight();
+      var bottomOfWindow = $(window).scrollTop() + $(window).height();
+      if (bottomOfWindow >= bottomOfRow) {
+        $(this).find(".fade-in").fadeIn("slow");
+      }
+    });
   });
 });
