26f4335289ea82cffd36d337d087ecdcdf93f881
Dan Kang <dan@dskang.com>
2017-03-11 18:54:27 +0900

    Add fixed header

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,11 @@
-// Javascript goes here!
+window.addEventListener('scroll', function(e) {
+  var siteHeader = document.getElementsByClassName('site-header')[0];
+  var fixedHeader = document.getElementsByClassName('fixed-header')[0];
+  var siteHeaderHeight = siteHeader.offsetHeight;
+  var scrolledPastHeader = document.body.scrollTop - siteHeaderHeight > 0;
+  if (scrolledPastHeader) {
+    fixedHeader.style.display = 'block';
+  } else {
+    fixedHeader.style.display = 'none';
+  }
+});
