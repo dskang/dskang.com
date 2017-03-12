e17d9067e337de1edcd6f3ebda0a8707858c73d9
Dan Kang <dan@dskang.com>
2017-03-12 12:22:34 +0900

    Use IDs instead of classes for headers

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function(e) {
-  var siteHeader = document.getElementsByClassName('site-header')[0];
-  var fixedHeader = document.getElementsByClassName('fixed-header')[0];
+  var siteHeader = document.getElementById('site-header');
+  var fixedHeader = document.getElementById('fixed-header');
   var siteHeaderHeight = siteHeader.offsetHeight;
   var scrolledPastHeader = document.body.scrollTop - siteHeaderHeight > 0;
   if (scrolledPastHeader) {
